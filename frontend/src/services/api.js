f4969590964b8b5e6bbc3ab24a97ea6035108445
chalini99 <chalinim9094@gmail.com>
2026-04-14 20:38:43 +0530

    ok done

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,17 +1,7 @@
-
 import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://leadmanage-06jt.onrender.com/api"
-});
-
-// attach token automatically
-API.interceptors.request.use((req) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    req.headers.Authorization = `Bearer ${token}`;
-  }
-  return req;
 });
 
 export default API;
