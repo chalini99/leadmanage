f1fcb1375f74af97d6b1a23bf778ccafd7f8bd89
chalini99 <chalinim9094@gmail.com>
2026-04-14 20:01:19 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -5,19 +5,17 @@ import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import leadRoutes from "./routes/leadRoutes.js";
 
-
-
-
 dotenv.config();
 
 const app = express();
 
 // Middleware
-
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/auth", authRoutes);
 app.use("/api/leads", leadRoutes);
+
 // Test route
 app.get("/", (req, res) => {
   res.send("API is running...");
@@ -29,9 +27,12 @@ const startServer = async () => {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
 
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    const PORT = process.env.PORT || 5000;
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
+
   } catch (error) {
     console.log(error);
   }
